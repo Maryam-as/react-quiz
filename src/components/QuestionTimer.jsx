8c8d2db031b31d510e01082a752b88f85a6c9467
Maryam-as <203218399+Maryam-as@users.noreply.github.com>
2025-05-01 12:28:05 +0330

    feat: implement countdown logic in QuestionTimer with setInterval and useEffect
    Using useEffect prevents setInterval from being re-initialized on every render, avoiding infinite loop issues.

--- src/components/QuestionTimer.jsx
+++ src/components/QuestionTimer.jsx
@@ -1,9 +1,15 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function QuestionTimer({ timeout, onTimeout }) {
   const [remainingTime, setRemainingTime] = useState(timeout);
 
   setTimeout(onTimeout, timeout);
+
+  useEffect(() => {
+    setInterval(() => {
+      setRemainingTime((prevRemainingTime) => prevRemainingTime - 100);
+    }, 100);
+  }, []);
 
   return <progress id="question-time" />;
 }
