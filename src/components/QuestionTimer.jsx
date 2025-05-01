2da5e262edc418e1da3569826cf79704c93772de
Maryam-as <203218399+Maryam-as@users.noreply.github.com>
2025-05-01 12:01:47 +0330

    feat: add timeout logic to QuestionTimer component

--- src/components/QuestionTimer.jsx
+++ src/components/QuestionTimer.jsx
@@ -1,3 +1,5 @@
-export default function QuestionTimer() {
+export default function QuestionTimer({ timeout, onTimeout }) {
+  setTimeout(onTimeout, timeout);
+
   return <progress id="question-time" />;
 }
